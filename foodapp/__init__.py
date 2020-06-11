815f68fe1836a4da4faede925227d5ccccf863f5
Sivaprasad <unni007prasad@gmail.com>
2020-06-12 00:20:36 +0530

    static and templates

--- foodapp/__init__.py
+++ foodapp/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
-app = Flask(__name__, template_folder='../templates')
+app = Flask(__name__, template_folder='templates')
 
 app.config['SECRET_KEY'] = '7976b37fadec64fb4ceae186f7533ec4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
