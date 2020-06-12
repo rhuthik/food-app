9f89236b2765ba5dc8c3a882c83966d57b79d783
Sivaprasad <unni007prasad@gmail.com>
2020-06-12 21:28:02 +0530

    README changed

---
+++ seed.py
@@ -0,0 +1,20 @@
+
+from foodapp import db
+from foodapp.models import Recipe, Ingredient
+import json
+
+if len(Recipe.query.all()) == 0:
+  db.create_all()
+
+with open('seed-resource/train.json') as json_file:
+    data = json.loads(json_file.read())
+    for recipe in data:
+        rec = Recipe(name=recipe['cuisine'], procedure="lorem ipsum")
+        db.session.add(rec)
+        db.session.commit()
+        for ing in recipe['ingredients']:
+            ingre = Ingredient(name=ing)
+            db.session.add(ingre)
+            db.session.commit()
+            rec.ingredients.append(ingre)
+            db.session.commit()
