6c824c4a6a92a3b7c3426843835eeb5eb08604d6
RayChithra <pranoy323@gmail.com>
2020-06-17 19:13:32 +0000

    Added migration

--- foodapp/__init__.py
+++ foodapp/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 import json
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__, template_folder='templates')
 
@@ -11,4 +13,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
 from foodapp import routes
+
+if __name__ == '__main__':
+	manager.run()
