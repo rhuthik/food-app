d9f884d9dd2fd8635ec55e9790f80f5eaf457a44
Rhuthik <rhuthikp13@gmail.com>
2020-06-18 01:22:56 +0530

    scrape2 updated

--- scrape2.py
+++ scrape2.py
@@ -2,6 +2,19 @@ from bs4 import BeautifulSoup
 import requests
 url = 'https://www.bbcgoodfood.com/recipes/collection/easy'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-source = requests.get('https://www.bbcgoodfood.com/recipes/collection/easy').text
+source = requests.get('https://www.bbcgoodfood.com/recipes/collection/easy',headers = headers).text
 soup = BeautifulSoup(source,'lxml')
-print(soup.prettify())
+#print(soup.prettify())
+#for article in soup.find_all('article'):
+    #src = article.h3.a['href']
+    #link = f'https://www.bbcgoodfood.com{src}'
+    #print(link)
+foodsource = requests.get('https://www.bbcgoodfood.com/recipes/chorizo-mozzarella-gnocchi-bake',headers = headers).text
+unique = BeautifulSoup(foodsource,'lxml')
+#print(unique.prettify())
+name = unique.find(class_='recipe-header__title').text
+#print(name)
+method = unique.find('div', class_='method').text
+#print(method)
+
+
