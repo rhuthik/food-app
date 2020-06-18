97a5deb602e4a4fccd85e7cf47de97d9666bbb49
Rhuthik <rhuthikp13@gmail.com>
2020-06-19 00:07:50 +0530

    scrape2 updated

--- scrape2.py
+++ scrape2.py
@@ -5,9 +5,10 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleW
 source = requests.get('https://www.bbcgoodfood.com/recipes/collection/easy',headers = headers).text
 soup = BeautifulSoup(source,'lxml')
 #print(soup.prettify())
-#for article in soup.find_all('article'):
-    #src = article.h3.a['href']
-    #link = f'https://www.bbcgoodfood.com{src}'
+for article in soup.find_all('article'):
+    src = article.h3.a['href']
+    link = f'https://www.bbcgoodfood.com{src}'
+    open('links.txt','wb').write(link.content)
     #print(link)
 foodsource = requests.get('https://www.bbcgoodfood.com/recipes/chorizo-mozzarella-gnocchi-bake',headers = headers).text
 unique = BeautifulSoup(foodsource,'lxml')
@@ -16,5 +17,12 @@ name = unique.find(class_='recipe-header__title').text
 #print(name)
 method = unique.find('div', class_='method').text
 #print(method)
+for check in unique.find_all(class_='ingredients-list__item'):
+    ingred = check['content']
+    ient = check.text
+    #print(ingred)
+#ingred = unique.find_all(class_='ingredients-list__item')['content']
+image = unique.find(itemprop='image')['src']
+#print(image)
 
 
