17613f934e9bedd16eb2cadc446f20fce267abe5
Rhuthik <rhuthikp13@gmail.com>
2020-06-19 13:22:25 +0530

    delete scrape.py

--- scrape.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup 
-import requests
-with open('test.html') as html_file:
-    soup =BeautifulSoup(html_file, 'lxml')
-
-article = soup.find('article')
-#print(article.prettify())
-headline = article.h3.a.text
-print(headline)
